41accb689576f7d57cd3707e667590b8306e4320
Arya Prast <wardhana.prast@gmail.com>
2016-01-05 17:02:34 +0700

    getEpp

--- Liquid.php
+++ Liquid.php
@@ -415,17 +415,16 @@ class Registrar_Adapter_Liquid extends Registrar_AdapterAbstract
         $result = $this->_makeRequest('domains/modify-privacy-protection', $params, 'POST');
         return (strtolower($result['actionstatus']) == 'success');
     }
+    
     public function getEpp(Registrar_Domain $domain)
     {
-        $params = array(
-            'order-id'      =>  $this->_getDomainOrderId($domain),
-            'options'       =>  'OrderDetails',
-        );
-        $data = $this->_makeRequest('domains/details', $params);
-        if(!isset($data['domsecret'])) {
+        $domain_id = $this->_getDomainOrderId($domain);
+        $auth_code = $this->_makeRequest('domains/'.$domain_id.'/auth_code');
+
+        if(empty($auth_code)) {
             throw new Registrar_Exception('Domain EPP code can be retrieved from domain registrar');
         }
-        return $data['domsecret'];
+        return $auth_code;
     }
     public function lock(Registrar_Domain $domain)
     {
